fb50bc3889b85fd25c90b9bd76c22d3944ca7d37
Juan Uchise <juan.uxz@gmail.com>
2024-06-26 22:03:29 -0300

    Create 13 - SELECT TOP 1000 LINHAS.sql
    SELECT TOP

---
+++ 13 - SELECT TOP 1000 LINHAS.sql
@@ -0,0 +1,13 @@
+USE ContosoRetailDW
+
+SELECT TOP 1000  -- USANDO O TOP + O NUMERO DE LINHAS RETORNA SOMENTE A QUANTIDADE DE LINHAS QUE ESCREVEMOS
+	EmailAddress
+	,Gender
+	,MaritalStatus
+	,TotalChildren
+	,NumberChildrenAtHome
+	,NumberCarsOwned AS 'QUANTIDADE_CARROS'
+	,Education
+FROM DimCustomer
+ 
+ORDER BY 2 DESC, 3 DESC, 4 DESC, [QUANTIDADE_CARROS] DESC
