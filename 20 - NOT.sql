4942d4c5cec10ef8af1d53222f4557a87447d5f6
Juan Uchise <juan.uxz@gmail.com>
2024-06-27 21:28:04 -0300

    Create 20 - NOT.sql
    NOT

---
+++ 20 - NOT.sql
@@ -0,0 +1,20 @@
+USE ContosoRetailDW
+
+SELECT
+	EmailAddress
+	,Gender
+	,MaritalStatus
+	,TotalChildren
+	,NumberChildrenAtHome
+	,NumberCarsOwned AS 'QUANTIDADE_CARROS'
+	,Education
+	,BirthDate
+FROM DimCustomer
+WHERE
+	MaritalStatus IS NOT NULL 
+	AND NOT TotalChildren BETWEEN 2 AND 4 
+	AND NOT BirthDate BETWEEN '1950-01-01' AND '1960-01-02'
+	AND NumberChildrenAtHome NOT IN (0, 1, 4) -- PODEMOS USAR O NOT PARA NEGAR AS CONSULTAS QUE FIZEMOS
+
+ORDER BY
+	NumberChildrenAtHome DESC
