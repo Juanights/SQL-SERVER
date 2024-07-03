da14cb6cea2ad4439c5cbdb5bd60a3d5e6427f65
Juan Uchise <juan.uxz@gmail.com>
2024-07-03 20:53:38 -0300

    Update 29 - MULTIPLE JOINS.sql

--- 29 - MULTIPLE JOINS.sql
+++ 29 - MULTIPLE JOINS.sql
@@ -10,7 +10,7 @@ SELECT
 	,C.ProductCategoryKey
 	,C.ProductCategoryName
 	
-FROM FactSales AS S
-INNER JOIN DimProduct AS P ON P.ProductKey = S.ProductKey
+FROM FactSales AS S                               -- PODEMOS FAZER VARIOS RELACIONAMENTOS AO MESMO TEMPO CONFORMA ABAIXO
+INNER JOIN DimProduct AS P ON P.ProductKey = S.ProductKey          
 INNER JOIN DimProductSubcategory AS SC ON SC.ProductSubcategoryKey = P.ProductSubcategoryKey
 INNER JOIN DimProductCategory AS C ON C.ProductCategoryKey = SC.ProductCategoryKey
