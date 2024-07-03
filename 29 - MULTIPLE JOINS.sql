d9e74bcecadd5cb864a561e751099910f9369a40
Juan Uchise <juan.uxz@gmail.com>
2024-07-03 20:52:52 -0300

    Create 29 - MULTIPLE JOINS.sql

---
+++ 29 - MULTIPLE JOINS.sql
@@ -0,0 +1,16 @@
+USE ContosoRetailDW
+
+SELECT
+	S.SalesKey
+	,S.SalesAmount
+	,S.ProductKey
+	,P.ProductName
+	,P.ProductSubcategoryKey
+	,SC.ProductSubcategoryName
+	,C.ProductCategoryKey
+	,C.ProductCategoryName
+	
+FROM FactSales AS S
+INNER JOIN DimProduct AS P ON P.ProductKey = S.ProductKey
+INNER JOIN DimProductSubcategory AS SC ON SC.ProductSubcategoryKey = P.ProductSubcategoryKey
+INNER JOIN DimProductCategory AS C ON C.ProductCategoryKey = SC.ProductCategoryKey
