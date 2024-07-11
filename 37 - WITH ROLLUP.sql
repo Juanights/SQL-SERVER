d38bd478d20ff638c77af6b4396e81ccde32dfb8
Juan Uchise <juan.uxz@gmail.com>
2024-07-10 21:12:38 -0300

    Create 37 - WITH ROLLUP.sql

---
+++ 37 - WITH ROLLUP.sql
@@ -0,0 +1,13 @@
+USE ContosoRetailDW
+
+SELECT TOP 1000
+	G.RegionCountryName
+	,ST.StoreName
+	,SUM(S.SalesQuantity) AS 'QTD TOTAL'
+	
+FROM FactSales AS S
+INNER JOIN DimStore AS ST ON ST.StoreKey = S.StoreKey
+INNER JOIN DimGeography AS G ON G.GeographyKey = ST.GeographyKey
+
+GROUP BY  G.RegionCountryName, ST.StoreName
+WITH ROLLUP
