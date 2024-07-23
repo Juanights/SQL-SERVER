df9abfefd544578183e7a11fe518448b870f39ad
Juan Uchise <juan.uxz@gmail.com>
2024-07-22 22:09:27 -0300

    Update 41 - SET DATE FORMAT.sql

---
+++ 41 - SET DATE FORMAT.sql
@@ -0,0 +1,16 @@
+USE ContosoRetailDW
+
+
+-- SET DATEFORMAT
+-- MDY: Mês, Dia, Ano
+-- DMY: Dia, Mês, Ano
+-- YMD: Ano, Mês, Dia
+-- YDM: Ano, Dia, Mês
+-- MYD: Mês, Ano, Dia
+-- DYM: Dia, Ano, Mês
+
+SET DATEFORMAT DMY -- USAMOS ESSA SINTAXE EM SQL 
+SELECT 
+	S.DateKey
+FROM FactOnlineSales AS S
+WHERE S.DateKey BETWEEN '01-12-2007' AND '31-12-2007'
