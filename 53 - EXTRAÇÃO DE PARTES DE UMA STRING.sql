cbf6ff7c8922caad9487d2312e9e2565cfd5f732
Juan Uchise <juan.uxz@gmail.com>
2024-08-13 16:47:32 -0300

    Update 53 - EXTRAÇÃO DE PARTES DE UMA STRING.sql

--- 53 - EXTRAÇÃO DE PARTES DE UMA STRING.sql
+++ 53 - EXTRAÇÃO DE PARTES DE UMA STRING.sql
@@ -1,4 +1,4 @@
 SELECT 
 	'SQL é mais legal que Power Bi',
-	SUBSTRING('SQL é mais legal que Power Bi',1,3)
+	SUBSTRING('SQL é mais legal que Power Bi',1,3) --FUNÇÃO SUBSTRING (FRASE PARA EXTRAÇÃO, INICIO DA EXTRAÇÃO, FINAL DA EXTRAÇÃO)
 	
